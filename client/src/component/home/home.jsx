8e8c69dacfbf57e1767d2fa5aa29c255bfb5baeb
unknown <aliopdandan@gmail.com>
2024-07-20 14:21:15 +0100

    f

--- client/src/component/home/home.jsx
+++ client/src/component/home/home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Home() {
     return (
         <div className="bg-violet-900 min-h-screen flex justify-center">
-            <div className="bg-violet-700 max-w-lg p-6 flex flex-col items-center m-3">
+            <div className="bg-violet-700 max-w-lg p-6 flex flex-col items-center m-3 rounded">
                 <div className="flex items-center gap-4 mb-4">
                     <input 
                         type="text" 
@@ -13,7 +13,7 @@ export function Home() {
                     <button className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600">Log-in</button>
                     <button className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600">Sign-up</button>
                 </div>
-                <div className="text-4xl">+</div>
+                <div className="text-4xl text-white bg-red-600 p-5 cursor-pointer rounded hover:bg-red-700">+</div>
             </div>
         </div>
     );

66e4e5b0f550d5e07fe734025f10010dc9c73336
unknown <aliopdandan@gmail.com>
2024-07-21 10:55:27 +0100

    f

--- client/src/component/home/home.jsx
+++ client/src/component/home/home.jsx
@@ -54,7 +54,7 @@ export function Home() {
                     <button className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600">Log-in</button>
                     <button className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600">Sign-up</button>
                 </div>
-                <Note></Note>
+                <Note post={post} />
                 <div 
                     className="text-4xl text-white bg-red-600 p-5 cursor-pointer rounded hover:bg-red-700 mt-6" 
                     onClick={newNoteNav}
--- client/src/component/home/note.jsx
+++ client/src/component/home/note.jsx
@@ -8,7 +8,7 @@ import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
 import Stack from '@mui/material/Stack';
 
-export function Note(){
+export function Note({ post }) {
     const [openMenu, setOpenMenu] = useState(null); // Tracks which menu is open
     const anchorRef = useRef({});
 
@@ -45,48 +45,48 @@ export function Note(){
         };
     }, []);
 
-    return(
+    return (
         <div className="mt-6 w-full flex flex-wrap gap-4">
             {post.length > 0 ? (
                 post.map((note, index) => (
-                <div key={index} className="bg-white p-4 rounded shadow min-w-[200px] max-w-[400px]">
-                    <div className="flex justify-between items-center">
-                        <h3 className="text-xl font-bold">{note.title}</h3>
-                        <Button
-                            ref={el => anchorRef.current[index] = el}
-                            aria-controls={openMenu === index ? 'menu-list-grow' : undefined}
-                            aria-haspopup="true"
-                            onClick={() => handleMenuToggle(index)}
-                        >
-                        ...
-                        </Button>
-                        <Popper
-                            open={openMenu === index}
-                            anchorEl={anchorRef.current[index]}
-                            role={undefined}
-                            transition
-                            disablePortal
+                    <div key={index} className="bg-white p-4 rounded shadow min-w-[200px] max-w-[400px]">
+                        <div className="flex justify-between items-center">
+                            <h3 className="text-xl font-bold">{note.title}</h3>
+                            <Button
+                                ref={el => anchorRef.current[index] = el}
+                                aria-controls={openMenu === index ? 'menu-list-grow' : undefined}
+                                aria-haspopup="true"
+                                onClick={() => handleMenuToggle(index)}
                             >
-                            {({ TransitionProps }) => (
-                            <Grow {...TransitionProps} style={{ transformOrigin: 'center top' }}>
-                            <Paper>
-                            <ClickAwayListener onClickAway={handleCloseMenu}>
-                            <MenuList autoFocusItem={openMenu === index} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                                                            <MenuItem onClick={() => alert(`Edit ${note.title} clicked`)}>Edit</MenuItem>
-                                                            <MenuItem onClick={() => alert(`Delete ${note.title} clicked`)}>Delete</MenuItem>
-                                                        </MenuList>
-                                                    </ClickAwayListener>
-                                                </Paper>
-                                            </Grow>
-                                        )}
-                                    </Popper>
-                                </div>
-                                <p dangerouslySetInnerHTML={{ __html: note.note }}></p>
-                            </div>
-                        ))
-                    ) : (
-                        <p>No notes available</p>
-                    )}
-                </div>
-    )
+                                ...
+                            </Button>
+                            <Popper
+                                open={openMenu === index}
+                                anchorEl={anchorRef.current[index]}
+                                role={undefined}
+                                transition
+                                disablePortal
+                            >
+                                {({ TransitionProps }) => (
+                                    <Grow {...TransitionProps} style={{ transformOrigin: 'center top' }}>
+                                        <Paper>
+                                            <ClickAwayListener onClickAway={handleCloseMenu}>
+                                                <MenuList autoFocusItem={openMenu === index} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                                    <MenuItem onClick={() => alert(`Edit ${note.title} clicked`)}>Edit</MenuItem>
+                                                    <MenuItem onClick={() => alert(`Delete ${note.title} clicked`)}>Delete</MenuItem>
+                                                </MenuList>
+                                            </ClickAwayListener>
+                                        </Paper>
+                                    </Grow>
+                                )}
+                            </Popper>
+                        </div>
+                        <p dangerouslySetInnerHTML={{ __html: note.note }}></p>
+                    </div>
+                ))
+            ) : (
+                <p>No notes available</p>
+            )}
+        </div>
+    );
 }

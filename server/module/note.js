fccdd95fcd5b81ee0f35373c064549c4409b40dd
unknown <aliopdandan@gmail.com>
2024-07-27 08:23:03 +0100

    f

--- server/module/note.js
+++ server/module/note.js
@@ -10,6 +10,10 @@ const noteSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  title:{
+    type: String,
+    required: true
+  }
 });
 
 const Note = mongoose.model("Note", noteSchema);
